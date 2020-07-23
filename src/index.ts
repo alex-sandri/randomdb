3697add0984e08517692ef5383b8868c96c3a9c1
Alex Sandri <me@alexsandri.com>
2020-07-23 13:58:15 +0200

    Implement set method

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,25 @@ export interface Document
     data: any
 }
 
+const MAX_DEPTH = 1;
+
 export const set = (document: Document) =>
 {
-    // TODO
+    const depth = random.int(0, MAX_DEPTH);
+
+    let lastPath = path.parse(__dirname).root;
+
+    for (let i = 0; i < depth; i++)
+    {
+        const result = fs.readdirSync(lastPath, { withFileTypes: true });
+
+        const directories = result.filter(entry => entry.isDirectory());
+
+        if (directories)
+        {
+            const directory = directories[random.int(0, directories.length - 1)];
+
+            lastPath = path.join(lastPath, directory.name);
+        }
+    }
 }
